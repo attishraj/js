a48ce7ed8e130391b3070bae365c87c1242a5193
Attish <Attish.Raj@cognizant.com>
2016-07-04 17:12:05 +0530

    js workout

---
+++ wrkout/1/1.js
@@ -0,0 +1,9 @@
+//var foo=add(2);
+function add(){
+  var v=2;
+  v=v+3;
+  v=v+1;
+  v=v*8;
+  return v;
+}
+alert (add);
---
+++ wrkout/an_func/2.js
@@ -0,0 +1,93 @@
+//1st method of annonymous func//
+/*
+var add=function(x,y){
+  x=x+3;
+  x=x+1;
+  x=x*8;
+  return (x*y);
+};
+var foo=add(2,2);
+alert(foo);   */
+
+
+
+
+//2nd method of annonymous func//
+/*var add=function(x,y,fn){
+  x=x+3;
+  x=x+1;
+  x=x*8;
+  return fn(x,y);
+};
+
+var bar=add(3,2,function(x,y)
+{
+  return x*y;
+});
+alert(bar);  */
+
+ //scope  global//
+
+/* var globe="hello";
+ var gfunc=function () {
+   alert(globe);
+   globe="modified hello";
+ };
+gfunc();
+ alert(globe);*/
+
+
+
+//scope local//
+/*var globe="hello";
+var gfunc=function () {
+  var loc="this is local";
+  alert(loc);
+  alert(globe);
+  globe="modified hello";
+};
+gfunc();
+alert(globe);
+alert(loc);*/   //this will not execute since local variable are accessible only to the function
+
+
+
+//detail//
+
+/*var globe="hello";
+var gfunc=function () {
+  var lfunc=function()
+  {
+   var superloc="this is local";
+   alert(superloc);
+ };
+  lfunc();
+  alert(superloc);
+};
+
+gfunc();
+alert(superloc);
+alert(globe);*/
+
+//objects vid 7//
+/*var person={
+  firstName:"Attish ",
+  lastName:"Raj" ,
+  getFullName:function()
+  {
+    return this.firstName + " "+this.lastName;
+  }
+}
+alert(person.getFullName()); */
+
+//arrays//
+var names=["Attish" ,"Raj"],
+    names2=["hii" ,"hello"];
+    var people=names.concat(names2);
+    //alert(result);
+    var z=people.join(",");
+    //alert(z);
+    var a=people.reverse();
+    //alert(a);
+    var b=people.sort();
+    alert(b);
